1982cc3bb89ef16a9f18d53c7345a0271c158d4c
Brent Murray <56763040+Brent-Murray@users.noreply.github.com>
2022-08-04 13:41:28 -0700

    Update pointnet2.py

--- models/pointnet2.py
+++ models/pointnet2.py
@@ -1,3 +1,5 @@
+# Adapted from https://github.com/pyg-team/pytorch_geometric/tree/master/examples
+
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import MLP, PointConv, fps, global_max_pool, radius
