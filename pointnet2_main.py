8cb49796b2fa22d49cc683d57c062cd7c83cb454
Brent Murray <56763040+Brent-Murray@users.noreply.github.com>
2022-08-23 11:14:04 -0700

    Updated Main Scripts
    Added ignore warnings
    
    changed all models to Net
    
    Added check multi gpu function
    
    Edited for simplicity

--- pointnet2_main.py
+++ pointnet2_main.py
@@ -1,15 +1,10 @@
 import logging
 import os
 import sys
+import warnings 
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
 import torch
-import torchvision
 from models.pointnet2 import Net
-from sklearn.metrics import classification_report, confusion_matrix
 from tensorboardX import SummaryWriter
 from torch_geometric.nn import DataParallel
 from torch_geometric.loader import DataLoader, DataListLoader
@@ -21,70 +16,69 @@ from utils.tools import (
     _init_,
     delete_files,
     make_confusion_matrix,
+    check_multi_gpu,
 )
 from utils.train import train, test
 
-# Path to datasets
-train_dataset_path = r"D:\MurrayBrent\data\RMF_ITD\PLOT_LAS\BUF_5M_SC\train\trainingsets\fps"
-val_dataset_path = r"D:\MurrayBrent\data\RMF_ITD\PLOT_LAS\BUF_5M_SC\val\trainingsets\fps"
-test_dataset_path = ""
-# test_dataset_path = r"D:\MurrayBrent\data\RMF_ITD\PLOT_LAS\BUF_5M_SC\test"
+warnings.filterwarnings('ignore')
 
-# Load pretrained model ("" if training)
-# pretrained = r"D:\MurrayBrent\git\point-dl\notebooks\checkpoints\PointCNN_2048_6\models\best_model.t7"
+# Path to datasets
+train_dataset_path = (
+    r"D:\MurrayBrent\data\RMF_ITD\PLOT_LAS\BUF_5M_SC\train\trainingsets\fps"
+)
+
+val_dataset_path = (
+    r"D:\MurrayBrent\data\RMF_ITD\PLOT_LAS\BUF_5M_SC\val\trainingsets\fps"
+)
+
+test_dataset_path = ""  # "" if no testing dataset
+
+# Load pretrained model ("" if trianing)
 pretrained = ""
 
 # Batch Size
-batch_size=8
+batch_size = 8
 
 # Number of augmentations
 num_augs = 10
 
-# max_points
-# 1024, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240,
-# 11264, 12288, 13312, 14336, 15360, 16384, 17408, 18432, 19456, 20480
+# Max points
 max_points_list = [1024, 2048, 3072, 4096, 5120, 6144, 7168, 8192, 9216, 10240]
 
 # Fields to include from pointcloud
-use_columns = ["intensity"]
-# use_columns = []
+use_columns = ["intensity"]  # [] if none
 
-# Classes: must be in same order as in data
-# classes = ["Con", "Dec"]
+# Classes: Must be in same order as in data
 classes = [
     "Jack Pine",
     "White Spruce",
     "Black Spruce",
-    # "Balsam Fir",
-    # "Eastern White Cedar",
     "American Larch",
     "Paper Birch",
     "Trembling Aspen",
 ]
 
 
-def main(pretrained="", augment=True, num_augs=num_augs):
+def main(model_n, pretrained=pretrained, augment=True, num_augs=num_augs):
+    # Iterate through max_points_list
     for max_points in tqdm(max_points_list, desc="Total: ", leave=False, colour="blue"):
-        # Model Name
-        model_name = f"PointNet2_{max_points}_{len(classes)}"
-    
+        # Set model name
+        model_name = model_n + f"_{max_points}_{len(classes)}"
+        
+        # Check for muliple GPU's
+        multi_gpu = check_multi_gpu()
+
         # Set up TensorBoard summary writer
+        # Change to something else
         boardio = SummaryWriter(log_dir="checkpoints/" + model_name)
         _init_(model_name)
-
+        
         # Set up logger
         textio = IOStream("checkpoints/" + model_name + "/run.log")
         textio.cprint(model_name)
 
-        # Check for multi GPU's
-        if torch.cuda.device_count() > 1:
-            multi_gpu = True
-        else:
-            multi_gpu = False
-
-
         # Get training, validation and test datasets
-        if train_dataset_path:
+        if train_dataset_path:  # training dataset
             train_data_path = os.path.join(train_dataset_path, str(max_points))
             trainset = PointCloudsInFiles(
                 train_data_path,
@@ -107,14 +101,18 @@ def main(pretrained="", augment=True, num_augs=num_augs):
 
                     # Concat training and augmented training datasets
                     trainset = torch.utils.data.ConcatDataset([trainset, aug_trainset])
+
             # Load training dataset
-            if multi_gpu is True:
-                train_loader = DataListLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
-            else:
-                train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
+            if multi_gpu is True:  # loader into multi gpus
+                train_loader = DataListLoader(
+                    trainset, batch_size=batch_size, shuffle=True, num_workers=0
+                )
+            else:  # loader into single gpu/cpu
+                train_loader = DataLoader(
+                    trainset, batch_size=batch_size, shuffle=True, num_workers=0
+                )
 
-
-        if val_dataset_path:
+        if val_dataset_path:  # validation dataset
             val_data_path = os.path.join(val_dataset_path, str(max_points))
             valset = PointCloudsInFiles(
                 val_data_path,
@@ -123,14 +121,18 @@ def main(pretrained="", augment=True, num_augs=num_augs):
                 max_points=max_points,
                 use_columns=use_columns,
             )
+
             # Load validation dataset
-            if multi_gpu is True:
-                val_loader = DataListLoader(valset, batch_size=batch_size, shuffle=False, num_workers=0)
-            else:
-                val_loader = DataLoader(valset, batch_size=batch_size, shuffle=False, num_workers=0)
+            if multi_gpu is True:  # loader for multi gpus
+                val_loader = DataListLoader(
+                    valset, batch_size=batch_size, shuffle=False, num_workers=0
+                )
+            else:  # loader for single gpu/cpu
+                val_loader = DataLoader(
+                    valset, batch_size=batch_size, shuffle=False, num_workers=0
+                )
 
-
-        if test_dataset_path:
+        if test_dataset_path:  # test dataset
             test_data_path = os.path.join(test_dataset_path, str(max_points))
             testset = PointCloudsInFiles(
                 test_data_path,
@@ -140,39 +142,46 @@ def main(pretrained="", augment=True, num_augs=num_augs):
                 use_columns=use_columns,
             )
             # Load testing dataset
-            if multi_gpu is True:
-                test_loader = DataListLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
-            else:
-                test_loader = DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
-
+            if multi_gpu is True:  # loader for multi gpus
+                test_loader = DataListLoader(
+                    testset, batch_size=batch_size, shuffle=False, num_workers=0
+                )
+            else:  # loader for single gpu/cpu
+                test_loader = DataLoader(
+                    testset, batch_size=batch_size, shuffle=False, num_workers=0
+                )
 
         # Define device
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        # Define model
+        # Define Model
         model = Net(num_classes=len(classes), num_features=len(use_columns))
-
-        # Checkpoint
-        checkpoint = None
 
         # Load existing model
         if pretrained:
             assert os.path.isfile(pretrained)
-            model.load_state_dict(torch.load(pretrained, map_location="cpu"))
-
+            model.load_state_dict(torch.load(pretrained, map_loaction="cpu"))
+        
         # Send model to defined device
         model.to(device)
         
         # Set DataParallel if Multiple GPUs available
         if multi_gpu is True:
-            print("Using Multiple GPUs")
-            model = DataParallel(model.cuda(), device_ids=list(range(0,torch.cuda.device_count())))
-
+            model = DataParallel(
+                model.cuda(), device_ids=list(range(0, torch.cuda.device_count()))
+            )
+        
         # Run testing
         if pretrained:
             finished = test(
-                device=device, model=model, test_loader=test_loader, classes=classes, textio=textio,)
+                device=device,
+                model=model,
+                test_loader=test_loader,
+                classes=classes,
+                textio=textio,
+            )
             return finished
+
         # Run training
         else:
             train(
@@ -184,10 +193,12 @@ def main(pretrained="", augment=True, num_augs=num_augs):
                 classes=classes,
                 boardio=boardio,
                 textio=textio,
-                checkpoint=checkpoint,
             )
+        
+        # Empty Cache
+        torch.cuda.empty_cache()
         
         
 # Runtime
 if __name__ == "__main__":
-    main()
+    main(model_n="PointNet2")
