255bfa54ad85e1671092e3d07e965be7c61214f2
Brent Murray <56763040+Brent-Murray@users.noreply.github.com>
2022-08-29 14:38:41 -0700

    Create classifier.py
    Create classifier model to keep classification the same throughout and to easily allow for changes between models

---
+++ models/classifier.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import MLP
+
+
+class Classifier(torch.nn.Module):
+    def __init__(self, model, num_classes):
+        super().__init__()
+        self.model = model  # model
+        self.num_classes = num_classes  # number of classes
+        # MLP Layers
+        self.mlp1 = MLP([1024, 1024, 1024, 512], dropout=0.5)
+        self.mlp2 = MLP([512, 512, 512, 256], dropout=0.5)
+        self.mlp3 = MLP(
+            [256, 256, 256, self.num_classes], dropout=0.5, batch_norm=False
+        )
+
+    def forward(self, data):
+        output = self.model(data)
+        output = self.mlp1(output)
+        output = self.mlp2(output)
+        output = self.mlp3(output)
+
+        return output
